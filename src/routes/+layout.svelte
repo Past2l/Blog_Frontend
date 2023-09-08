9859af6c81d2305450f043e2aeee96ac7bce2349
Dohyeon Yun <to@dohyun.it>
2023-09-08 18:20:20 +0900

    add: `svelte-body` module

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import { deleteCookie } from '../module/cookie';
 	import { themeColor } from '../store/theme';
 	import { user } from '../store/user';
+	import { style } from 'svelte-body';
 	import '../styles/global.css';
 
 	export let data;
@@ -11,21 +12,16 @@
 	user.set(data?.user || null);
 	if (browser && data?.user) deleteCookie('Authorization');
 
-	$: style = Object.entries($themeColor || {})
+	$: themeStyle = Object.entries($themeColor || {})
 		.filter(([key]) => key != 'name')
 		.map(([key, value]) => `--${key.replace(/_/g, '-')}:${value}`)
 		.join(';\n');
 </script>
 
 <svelte:head />
-<main {style}>
+<svelte:body use:style={themeStyle} />
+
+<main>
 	<Header />
 	<slot />
 </main>
-
-<style>
-	main {
-		background: var(--input-background);
-		color: var(--text);
-	}
-</style>
